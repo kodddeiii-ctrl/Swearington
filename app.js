828e32834a45a494f25c6b344a299c458a88aac9
kodddeiii-ctrl <kodddeiii@gmail.com>
2026-08-25 16:41:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ client.on('messageCreate', async (message) => {
     }
     if (newPoints > 0) {
         const count = await getPoints(userId, guildId);
-        message.reply(`✨ Mind your language, angel! ${message.author.displayName} now has ${count} swear${count !== 1 ? 's' : ''}.`);
+        message.reply(`uh oh! ${message.author.displayName} now has ${count} swear${count !== 1 ? 's' : ''}.`);
     }
 });
 
